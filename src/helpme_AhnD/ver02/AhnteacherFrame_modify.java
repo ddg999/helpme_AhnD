6588dc0b0abb9ffe95c73e36bb260473a7aec660
kyeonghooon <gibbon0510@gmail.com>
2024-05-10 14:09:32 +0900

    메인프레임수정

---
+++ src/helpme_AhnD/ver02/AhnteacherFrame_modify.java
@@ -0,0 +1,121 @@
+package helpme_AhnD.ver02;
+
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/*
+ * 화면 구성 - 제목, (버튼(시작, 종료)), 설정
+ * 이벤트 - 입력 키(키보드, 마우스) 입력 받기
+ * 
+ * 제목 타이틀 이미지 나중에 해결하기 지금은 배경 이미지 
+ * 버튼 라벨로 나중에 수정하기 
+ * 인터페이스 파일... 
+ */
+public class AhnteacherFrame_modify extends JFrame {
+
+	// 컨텍스트를 생성하는 방법 (셀프 참조)
+	AhnteacherFrame_modify mContext = this; // 대기
+
+	// 버튼
+	private JLabel startButton;
+	private JLabel exitButton;
+	private JLabel setting;
+
+	private JLabel backgroundLabel;
+
+	public AhnteacherFrame_modify() {
+		
+		initData();
+		setInitLayout();
+		addEventListener();
+
+	}
+	
+	private void initData() {
+		backgroundLabel = new JLabel(new ImageIcon("images/mainFrame/bg.jpg"));
+		startButton = new JLabel(new ImageIcon("images/mainFrame/buttonStart.png"));
+		exitButton = new JLabel(new ImageIcon("images/mainFrame/buttonExit.png"));
+		setting = new JLabel(new ImageIcon("images/mainFrame/set1.png"));
+		setTitle("리듬 게임 시작 화면");
+		setSize(1600, 900); // 화면 사이즈
+		setContentPane(backgroundLabel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 게임 창을 종료 시 프로그램 전체 종료
+	}
+	private void setInitLayout() {
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null); // 화면 중앙에 프레임 위치
+		setVisible(true);
+		
+		add(startButton);
+		startButton.setSize(368, 124);
+		startButton.setLocation(616, 500);
+		
+		add(exitButton);
+		exitButton.setSize(368, 124);
+		exitButton.setLocation(616, 650);
+		
+		add(setting);
+		setting.setSize(50, 50);
+		setting.setLocation(1500, 800);
+	}
+	
+	private void addEventListener() {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch(e.getKeyCode()) {
+					case KeyEvent.VK_F1 :
+						startButton.setIcon(new ImageIcon("images/mainFrame/started3.png"));
+						break;
+					case KeyEvent.VK_F2 :
+						exitButton.setIcon(new ImageIcon("images/mainFrame/exited3.png"));
+						break;
+					case KeyEvent.VK_ESCAPE :
+						break;
+				}
+			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch(e.getKeyCode()) {
+					case KeyEvent.VK_F1 :
+						setVisible(false);
+						new GamePanel_kh();
+						break;
+					case KeyEvent.VK_F2 :
+						setVisible(false);
+						break;
+				}
+			}
+		});
+	}
+	private void startPressed() {
+		backgroundLabel.setIcon(new ImageIcon("images/mainFrame/bg2.jpg"));
+		remove(startButton);
+		remove(exitButton);
+		remove(setting);
+		
+	}
+	
+	
+	// 코드 테스트
+	public static void main(String[] args) {
+		new AhnteacherFrame_modify();
+	}
+
+}
