aac77cf1b364a50b6c6e37728a265b5d41200225
home <gibbon0510@gmail.com>
2024-05-12 17:45:57 +0900

    드랍노트 판정구현

---
+++ src/helpme_AhnD/ver02/components/ComboBox_kh.java
@@ -0,0 +1,51 @@
+package helpme_AhnD.ver02.components;
+
+import javax.swing.JLabel;
+
+import helpme_AhnD.ver02.service.PlayerService_kh;
+
+public class ComboBox_kh extends JLabel implements Runnable {
+
+	PlayerService_kh playerService;
+
+	// TODO 1p 2p 콤보박스 좌표설정
+//	private int x; 
+//	private int y;
+	private boolean isRun = true; // 게임 실행중
+
+	public ComboBox_kh(PlayerService_kh playerService) {
+		this.playerService = playerService;
+		initData();
+		setInitLayout();
+	}
+
+	public void initData() {
+//		x = 100;
+//		y = 200;
+	}
+
+	public void setInitLayout() {
+		setSize(100, 100);
+		setLocation(100, 200);
+		playerService.getmContext().add(this);
+	}
+
+	@Override
+	public void run() {
+		while (isRun) {
+			setText("<html><body text='white'><h2>" + playerService.getScore().getCombo()
+					+ " combo</h2></body></html>");
+//			// 테스트
+//			System.out.print("3점 : " + playerService.getScore().getCountExcellent() + ", ");
+//			System.out.print("2점 : " + playerService.getScore().getCountGreat() + ", ");
+//			System.out.print("1점 : " + playerService.getScore().getCountGood() + ", ");
+//			System.out.print("0점 : " + playerService.getScore().getCountBad() + ", ");
+//			System.out.println();
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- src/helpme_AhnD/ver02/service/Score.java
+++ src/helpme_AhnD/ver02/service/Score.java
@@ -8,8 +8,8 @@ public class Score {
 	private int hp;
 	private int score;
 	private int combo;
+	private int countPerfect;
 	private int countExcellent;
-	private int countGreat;
 	private int countGood;
 	private int countBad;
 
@@ -17,8 +17,8 @@ public class Score {
 		hp = 100;
 		score = 0;
 		combo = 0;
+		countPerfect = 0;
 		countExcellent = 0;
-		countGreat = 0;
 		countGood = 0;
 		countBad = 0;
 	}
@@ -31,16 +31,16 @@ public class Score {
 		}
 	}
 
-	public void excellent() {
+	public void perfect() {
 		score += 3;
 		combo++;
 		countExcellent++;
 	}
 
-	public void great() {
+	public void excellent() {
 		score += 2;
 		combo++;
-		countGreat++;
+		countPerfect++;
 	}
 
 	public void good() {
@@ -88,11 +88,11 @@ public class Score {
 	}
 
 	public int getCountGreat() {
-		return countGreat;
+		return countPerfect;
 	}
 
 	public void setCountGreat(int countGreat) {
-		this.countGreat = countGreat;
+		this.countPerfect = countGreat;
 	}
 
 	public int getCountGood() {
